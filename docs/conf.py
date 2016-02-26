ac727f8c489b8da20427b0a59bb0ca355474a713
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-02-26 20:23:43 +0900

    update docs/conf.ph

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,12 @@ sys.path.insert(0, os.path.abspath('../simplesqlite'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -65,7 +70,7 @@ release = u'0.1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
