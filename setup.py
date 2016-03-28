db19ee1f827f2b52210cfd1ac9a500370da0974e
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-03-28 21:42:51 +0900

    Update seup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="SimpleSQLite",
-    version="0.2.2",
+    version="0.2.3",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/SimpleSQLite",
@@ -41,7 +41,6 @@ setuptools.setup(
         "Operating System :: POSIX",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
