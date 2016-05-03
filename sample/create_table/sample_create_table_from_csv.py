e487f33df78ad80eef11bef1534b853f8077d927
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-05-03 19:36:22 +0900

    Update sample

--- sample/create_table/sample_create_table_from_csv.py
+++ sample/create_table/sample_create_table_from_csv.py
@@ -6,10 +6,19 @@ from simplesqlite import SimpleSQLite
 import six
 
 
-table_name = "sample_data"
-con = SimpleSQLite("sample.sqlite", "w")
-con.create_table_from_csv(csv_path="sample_data.csv")
+file_path = "sample_data.csv"
 
+with open(file_path, "w") as f:
+    f.write(""""attr_a","attr_b","attr_c"
+1,4,"a"
+2,2.1,"bb"
+3,120.9,"ccc"
+""")
+
+con = SimpleSQLite("sample.sqlite", "w")
+con.create_table_from_csv(file_path)
+
+table_name = "sample_data"
 six.print_(con.get_attribute_name_list(table_name))
 result = con.select(select="*", table_name=table_name)
 for record in result.fetchall():
