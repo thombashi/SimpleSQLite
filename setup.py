431a112d2460301ef9c961fd4081cb2418592044
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-11-13 22:57:47 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="SimpleSQLite",
-    version="0.6.3",
+    version="0.6.4",
     url="https://github.com/thombashi/SimpleSQLite",
 
     author="Tsuyoshi Hombashi",
