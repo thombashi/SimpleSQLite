d255785acedf2f7cf6c7ae7950712591b8b0f45d
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-25 16:01:08 +0900

    Initial commit

---
+++ sample/sample_get_sqlite_master.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import print_function
+import json
+
+from simplesqlite import SimpleSQLite
+
+con = SimpleSQLite("sample.sqlite", "w")
+data_matrix = [
+    [1, 1.1, "aaa", 1,   1],
+    [2, 2.2, "bbb", 2.2, 2.2],
+    [3, 3.3, "ccc", 3,   "ccc"],
+]
+con.create_table_from_data_matrix(
+    table_name="sample_table",
+    attr_name_list=["a", "b", "c", "d", "e"],
+    data_matrix=data_matrix,
+    index_attr_list=["a"])
+
+print(json.dumps(con.get_sqlite_master(), indent=4))
