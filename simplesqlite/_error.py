df01738be877a3f63318e8736c9252e7e33810ac
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-31 10:37:47 +0900

    Update docstrings

--- simplesqlite/_error.py
+++ simplesqlite/_error.py
@@ -13,13 +13,13 @@ class DatabaseError(sqlite3.DatabaseError):
 
     .. seealso::
 
-        - `sqlite3.DatabaseError <https://docs.python.org/3.5/library/sqlite3.html#sqlite3.DatabaseError>`__
+        - `sqlite3.DatabaseError <https://docs.python.org/3/library/sqlite3.html#sqlite3.DatabaseError>`__
     """
 
 
 class NullDatabaseConnectionError(Exception):
     """
-    Raised when executing an operation of
+    Exception raised when executing an operation of
     :py:class:`~simplesqlite.SimpleSQLite` instance without connection to
     a SQLite database file.
     """
@@ -27,35 +27,35 @@ class NullDatabaseConnectionError(Exception):
 
 class TableNotFoundError(Exception):
     """
-    Raised when accessed the table that not exists in the database.
+    Exception raised when accessed the table that not exists in the database.
     """
 
 
 class AttributeNotFoundError(Exception):
     """
-    Raised when accessed the attribute that not exists in the table.
+    Exception raised when accessed the attribute that not exists in the table.
     """
 
 
 class InvalidTableNameError(ValueError):
     """
-    Raised when used invalid table name for SQLite.
+    Exception raised when used invalid table name for SQLite.
     """
 
 
 class InvalidAttributeNameError(ValueError):
     """
-    Raised when used invalid attribute name for SQLite.
+    Exception raised when used invalid attribute name for SQLite.
     """
 
 
 class SqlSyntaxError(Exception):
     """
-    Raised when a SQLite query syntax is invalid.
+    Exception raised when a SQLite query syntax is invalid.
     """
 
 
 class OperationalError(sqlite3.OperationalError):
     """
-    Raised when failed to execute a query
+    Exception raised when failed to execute a query.
     """
