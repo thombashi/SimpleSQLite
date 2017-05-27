31bdb5e4b9e74a129c18dfa2573272efba397f25
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-05-27 22:32:54 +0900

    Update an example

--- sample/select_as_dataframe.py
+++ sample/select_as_dataframe.py
@@ -8,16 +8,15 @@ from simplesqlite import SimpleSQLite
 
 con = SimpleSQLite("sample.sqlite", "w", profile=True)
 header_list = ["a", "b", "c", "d", "e"]
-data_matrix = [
-    [1, 1.1, "aaa", 1,   1],
-    [2, 2.2, "bbb", 2.2, 2.2],
-    [3, 3.3, "ccc", 3,   "ccc"],
-]
 
 con.create_table_from_data_matrix(
     table_name="sample_table",
     attr_name_list=header_list,
-    data_matrix=data_matrix)
+    data_matrix=[
+        [1, 1.1, "aaa", 1,   1],
+        [2, 2.2, "bbb", 2.2, 2.2],
+        [3, 3.3, "ccc", 3,   "ccc"],
+    ])
 
 print(con.select_as_dataframe(
     column_list=header_list, table_name="sample_table"))
