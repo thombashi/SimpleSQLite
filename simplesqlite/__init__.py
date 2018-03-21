48bfdcc81ef18a5cd3d7e89b6ae0dcb577cda8bc
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-03-21 20:52:48 +0900

    Apply isort

--- simplesqlite/__init__.py
+++ simplesqlite/__init__.py
@@ -7,25 +7,8 @@
 from __future__ import absolute_import
 
 from ._error import (
-    DatabaseError,
-    NullDatabaseConnectionError,
-    TableNotFoundError,
-    AttributeNotFoundError,
-    InvalidTableNameError,
-    InvalidAttributeNameError,
-    SqlSyntaxError,
-    OperationalError,
-)
-from ._func import (
-    append_table,
-    copy_table,
-)
-from ._logger import (
-    set_logger,
-    set_log_level,
-)
-from .core import (
-    connect_sqlite_memdb,
-    connect_sqlite_db_mem,
-    SimpleSQLite,
-)
+    AttributeNotFoundError, DatabaseError, InvalidAttributeNameError, InvalidTableNameError,
+    NullDatabaseConnectionError, OperationalError, SqlSyntaxError, TableNotFoundError)
+from ._func import append_table, copy_table
+from ._logger import set_log_level, set_logger
+from .core import SimpleSQLite, connect_sqlite_db_mem, connect_sqlite_memdb
