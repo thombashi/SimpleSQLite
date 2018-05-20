af4552d47bd69c877d88f738f3ac98448d7642e3
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-20 23:06:40 +0900

    Bump version to 0.20.4

--- simplesqlite/__version__.py
+++ simplesqlite/__version__.py
@@ -6,6 +6,6 @@ from datetime import datetime
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = "Copyright {}, {}".format(datetime.now().year, __author__)
 __license__ = "MIT License"
-__version__ = "0.20.3"
+__version__ = "0.20.4"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
