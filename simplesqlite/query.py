8046d8b82b1e956c106a82faa4c77508349dfb9f
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-27 13:09:51 +0900

    Add query classes

---
+++ simplesqlite/query.py
@@ -0,0 +1,341 @@
+# encoding: utf-8
+
+'''
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import abc
+import re
+
+import pathvalidate as pv
+import six
+import typepy
+
+from ._func import validate_table_name
+from .error import SqlSyntaxError
+
+
+@six.add_metaclass(abc.ABCMeta)
+class QueryItemInterface(object):
+
+    @abc.abstractmethod
+    def to_query(self):  # pragma: no cover
+        pass
+
+
+class QueryItem(QueryItemInterface):
+
+    def __init__(self, value):
+        try:
+            self._value = value.strip()
+        except AttributeError:
+            raise TypeError("name must be a string")
+
+    def __repr__(self):
+        return self.to_query()
+
+    def __format__(self, format_spec):
+        return self.to_query()
+
+
+class Table(QueryItem):
+    """
+    :param str name: Table name.
+    :return: String that suitable for table name of a SQLite query.
+
+    :Examples:
+        >>> from simplesqlite.query import Table
+        >>> Table("length")
+        'length'
+        >>> Table("length(cm)")
+        '[length(cm)]'
+        >>> Table("string length")
+        "'string length'"
+    """
+
+    __RE_NEED_BRACKET = re.compile("[{:s}]".format(re.escape("%()-+/.,")))
+    __RE_NEED_QUOTE = re.compile("[\s]+")
+
+    def to_query(self):
+        name = self._value
+
+        if self.__RE_NEED_BRACKET.search(name):
+            return "[{:s}]".format(name)
+
+        if self.__RE_NEED_QUOTE.search(name):
+            return "'{:s}'".format(name)
+
+        return name
+
+
+class Attr(QueryItem):
+    """
+    :param str name: Attribute name.
+    :param str operation:
+        Used as a SQLite function if the value is not empty.
+    :return: String that suitable for attribute name of a SQLite query.
+    :rtype: str
+
+    :Examples:
+        >>> from simplesqlite.query import Attr
+        >>> Attr("key")
+        'key'
+        >>> Attr("a+b")
+        '[a+b]'
+        >>> Attr("key", operation="SUM")
+        'SUM(key)'
+    """
+
+    __RE_NEED_QUOTE = re.compile("[{:s}]".format(re.escape("[]_")))
+    __RE_NEED_BRACKET = re.compile(
+        "[{:s}0-9\s]".format(re.escape("%(){}-+/.;:`'\"\0\\*?<>|!#&=~^@")))
+    __RE_SANITIZE = re.compile("[{:s}]".format(re.escape("'\",")))
+
+    @classmethod
+    def sanitize(cls, name):
+        return cls.__RE_SANITIZE.sub("_", name)
+
+    def __init__(self, name, operation=""):
+        super(Attr, self).__init__(name)
+
+        self.__operation = operation
+
+    def to_query(self):
+        #name = self._value
+        name = self.sanitize(self._value)
+
+        need_quote = self.__RE_NEED_QUOTE.search(name) is not None
+
+        try:
+            pv.validate_sqlite_attr_name(name)
+        except pv.InvalidReservedNameError:
+            need_quote = True
+        except (pv.ValidReservedNameError, pv.NullNameError, pv.InvalidCharError):
+            pass
+
+        if need_quote:
+            sql_name = '"{:s}"'.format(name)
+        elif self.__RE_NEED_BRACKET.search(name):
+            sql_name = "[{:s}]".format(name)
+        elif name == "join":
+            sql_name = "[{:s}]".format(name)
+        else:
+            sql_name = name
+
+        if typepy.is_not_null_string(self.__operation):
+            sql_name = "{:s}({:s})".format(self.__operation, sql_name)
+
+        return sql_name
+
+
+class AttrList(list, QueryItemInterface):
+    """
+    :param list/tuple name_list: List of attribute names.
+    :param str operation:
+        Used as a SQLite function if the value is not empty.
+
+    :Examples:
+        >>> from simplesqlite.query import AttrList
+        >>> AttrList(["key", "a+b"]))
+        ['key', '[a+b]']
+        >>> AttrList(["key", "a+b"], operation="AVG")
+        ['AVG(key)', 'AVG([a+b])']
+
+    .. seealso::
+        :py:class:`Attr`
+    """
+
+    @classmethod
+    def sanitize(self, name_list):
+        return [Attr.sanitize(name) for name in name_list]
+
+    def __init__(self, name_list, operation=""):
+        try:
+            super(AttrList, self).__init__([Attr(name, operation) for name in name_list])
+        except AttributeError:
+            raise TypeError("name must be a string")
+
+    def __repr__(self):
+        return self.to_query()
+
+    def __format__(self, format_spec):
+        return self.to_query()
+
+    def to_query(self):
+        return ",".join([six.text_type(attr) for attr in self])
+
+
+class Value(QueryItem):
+    """
+    :param str value: Value associated with a key.
+    :return:
+        String that suitable for a value of a key.
+        Return ``"NULL"`` if the value is |None|.
+    :rtype: str
+
+    :Examples:
+        >>> from simplesqlite.query import Value
+        >>> Value(1.2)
+        '1.2'
+        >>> Value("value")
+        "'value'"
+        >>> Value(None)
+        'NULL'
+    """
+
+    def __init__(self, value):
+        self._value = value
+
+    def to_query(self):
+        value = self._value
+
+        if value is None:
+            return "NULL"
+
+        if typepy.Integer(value).is_type() or typepy.RealNumber(value).is_type():
+            return six.text_type(value)
+
+        try:
+            if value.find("'") != -1:
+                return '"{}"'.format(value)
+        except (TypeError, AttributeError):
+            pass
+
+        return "'{}'".format(value)
+
+
+class Where(QueryItem):
+    """
+    Make part of WHERE query.
+
+    :param str key: Attribute name of the key.
+    :param str value: Value of the right hand side associated with the key.
+    :param str cmp_operator: Comparison  operator of WHERE query.
+    :raises simplesqlite.SqlSyntaxError:
+        If **a)** ``cmp_operator`` is invalid operator. Valid operators are as follows:
+        ``"="``, ``"=="``, ``"!="``, ``"<>"``, ``">"``, ``">="``, ``"<"``, ``"<="``.
+        **b)** the ``value`` is |None| and the ``cmp_operator`` is not ``"="``/``"!="``.
+
+    :Examples:
+        >>> from simplesqlite.query import Where
+        >>> Where("key", "hoge")
+        "key = 'hoge'"
+        >>> Where("value", 1, cmp_operator=">")
+        'value > 1'
+    """
+
+    __VALID_CMP_OPERATOR_LIST = ("=", "==", "!=", "<>", ">", ">=", "<", "<=")
+
+    @property
+    def key(self):
+        return self._value
+
+    @property
+    def value(self):
+        return self.__rhs
+
+    def __init__(self, key, value, cmp_operator="="):
+        super(Where, self).__init__(key)
+
+        self.__rhs = value
+        self.__cmp_operator = cmp_operator
+
+        if not self.__cmp_operator:
+            raise SqlSyntaxError("cmp_operator required")
+
+        if self.__cmp_operator not in self.__VALID_CMP_OPERATOR_LIST:
+            raise SqlSyntaxError("operator not supported: {}".format(self.__cmp_operator))
+
+    def to_query(self):
+        if self.value is None:
+            if self.__cmp_operator == "=":
+                return "{} IS NULL".format(Attr(self.key))
+            elif self.__cmp_operator == "!=":
+                return "{} IS NOT NULL".format(Attr(self.key))
+
+            raise SqlSyntaxError(
+                "Invalid operator ({:s}) with None right-hand side".format(
+                    self.__cmp_operator))
+
+        return "{} {:s} {}".format(Attr(self.key), self.__cmp_operator, Value(self.value))
+
+
+class Select(QueryItem):
+    """
+    Make SELECT query.
+
+    :param str select: Attribute for SELECT query.
+    :param str table: Table name of executing the query.
+    :param str where:
+        Add a WHERE clause to execute query, if the value is not |None|.
+    :param extra extra:
+        Add additional clause to execute query, if the value is not |None|.
+    :raises ValueError: ``select`` is empty string.
+    :raises simplesqlite.InvalidTableNameError:
+        |raises_validate_table_name|
+
+    :Examples:
+        >>> from simplesqlite.query import Select, Where
+        >>> Select(select="value", table="example")
+        'SELECT value FROM example'
+        >>> Select(select="value", table="example", where=Where("key", 1))
+        'SELECT value FROM example WHERE key = 1'
+        >>> Select(select="value", table="example", where=Where("key", 1), extra="ORDER BY value")
+        'SELECT value FROM example WHERE key = 1 ORDER BY value'
+    """
+
+    def __init__(self, select, table, where=None, extra=None):
+        self.__select = select
+        self.__table = table
+        self.__where = where
+        self.__extra = extra
+
+        validate_table_name(self.__table)
+
+        if not self.__select:
+            raise ValueError("SELECT query required")
+
+    def to_query(self):
+        query_list = [
+            "SELECT {}".format(self.__select),
+            "FROM {}".format(Table(self.__table)),
+        ]
+
+        if self.__where:
+            query_list.append("WHERE {}".format(self.__where))
+        if self.__extra:
+            query_list.append(self.__extra)
+
+        return " ".join(query_list)
+
+
+class And(list, QueryItemInterface):
+
+    def __init__(self, where_list):
+        for where in where_list:
+            if not isinstance(where, (six.text_type, Where)):
+                raise TypeError(
+                    "where clause item must be either string or Where class instance")
+
+        super(And, self).__init__(where_list)
+
+    def __repr__(self):
+        return self.to_query()
+
+    def __format__(self, format_spec):
+        return self.to_query()
+
+    def to_query(self):
+        return " AND ".join([six.text_type(where) for where in self])
+
+
+def make_index_name(table_name, attr_name):
+    import hashlib
+
+    sanitize = re.compile("[^a-zA-Z0-9_]")
+    index_hash = hashlib.md5(six.b(table_name + attr_name)).hexdigest()[:4]
+
+    return "{:s}_{:s}_index_{}".format(
+        sanitize.sub("", table_name), sanitize.sub("", attr_name), index_hash)
---
+++ test/test_query.py
@@ -0,0 +1,244 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import os
+import re
+import string
+
+import pytest
+import six
+from simplesqlite import InvalidTableNameError, SqlSyntaxError
+from simplesqlite.query import And, Attr, AttrList, Select, Table, Value, Where, make_index_name
+
+
+nan = float("nan")
+inf = float("inf")
+
+
+def assert_query_item(item, expected):
+    assert item.to_query() == expected
+    assert six.text_type(item) == expected
+    assert "{}".format(item) == expected
+
+
+class Test_Table(object):
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["test", "test"],
+        ["te%st", "[te%st]"],
+        ["te(st", "[te(st]"],
+        ["te)st", "[te)st]"],
+        ["te-st", "[te-st]"],
+        ["te+st", "[te+st]"],
+        ["te.st", "[te.st]"],
+        ["te,st", "[te,st]"],
+        ["te st", "'te st'"],
+    ])
+    def test_normal(self, value, expected):
+        assert_query_item(Table(value), expected)
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, TypeError],
+        [1, TypeError],
+        [False, TypeError],
+    ])
+    def test_exception(self, value, expected):
+        with pytest.raises(expected):
+            assert "{}".format(Table(value))
+
+
+class Test_Attr(object):
+
+    @pytest.mark.parametrize(["value", "operation", "expected"], [
+        ["test", None, "test"],
+        ["te'st", None, '"te_st"'],
+        ['te"st', None, '"te_st"'],
+        ['te,st', None, '"te_st"'],
+        ['te.st', None, '[te.st]'],
+        ["test", "AVG", "AVG(test)"],
+        ["attr_a", 2, '"attr_a"'],
+        ["attr_a", True, '"attr_a"'],
+        ["ABCD>8.5", None, "[ABCD>8.5]"],
+    ] + [
+        [
+            "te{:s}st".format(re.escape(c)),
+            None,
+            "[te{:s}st]".format(re.escape(c)),
+        ]
+        for c in string.digits + "%(). -+#"
+    ])
+    def test_normal(self, value, operation, expected):
+        assert_query_item(Attr(value, operation), expected)
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, TypeError],
+        [1, TypeError],
+        [False, TypeError],
+    ])
+    def test_exception_1(self, value, expected):
+        with pytest.raises(expected):
+            "{}".format(Attr(value))
+
+
+class Test_AttrList(object):
+
+    @pytest.mark.parametrize(
+        ["value", "operation", "expected"],
+        [
+            [
+                ["%aaa", "bbb", "ccc-ddd"],
+                None,
+                "[%aaa],bbb,[ccc-ddd]",
+            ],
+            [
+                ["%aaa", "bbb"],
+                "SUM",
+                "SUM([%aaa]),SUM(bbb)",
+            ],
+            [[], None, ""],
+        ])
+    def test_normal(self, value, operation, expected):
+        assert_query_item(AttrList(value, operation), expected)
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, TypeError],
+        [nan, TypeError],
+        [True, TypeError],
+    ])
+    def test_exception(self, value, expected):
+        with pytest.raises(expected):
+            AttrList(value)
+
+
+class Test_Value(object):
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [0, "0"],
+        ["0", "0"],
+        [1.1, "1.1"],
+        ["test", "'test'"],
+        ["te st", "'te st'"],
+        ["I'm", '"I\'m"'],
+        [None, "NULL"],
+        [False, "'False'"],
+    ])
+    def test_normal(self, value, expected):
+        assert_query_item(Value(value), expected)
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [nan, "'nan'"],
+        [inf, "'inf'"],
+    ])
+    def test_abnormal(self, value, expected):
+        assert_query_item(Value(value), expected)
+
+
+class Test_Where(object):
+
+    @pytest.mark.parametrize(["key", "value", "operation", "expected"], [
+        ["tkey", "tvalue", "=", "tkey = 'tvalue'"],
+        ["key key", "tvalue", "!=", "[key key] != 'tvalue'"],
+        ["%key+key", 100, "<", "[%key+key] < 100"],
+        ["key.key", "555", ">", "[key.key] > 555"],
+
+        ["tkey", None, "=", "tkey IS NULL"],
+        ["tkey", None, "!=", "tkey IS NOT NULL"],
+    ])
+    def test_normal(self, key, value, operation, expected):
+        assert_query_item(Where(key, value, operation), expected)
+
+    @pytest.mark.parametrize(["key", "value", "operation", "expected"], [
+        ["tkey", "tvalue", None, SqlSyntaxError],
+        ["tkey", "tvalue", "INVALID_VALUE", SqlSyntaxError],
+    ])
+    def test_exception(self, key, value, operation, expected):
+        with pytest.raises(expected):
+            "{}".format(Where(key, value, operation))
+
+
+class Test_Select(object):
+
+    @pytest.mark.parametrize(
+        ["select", "table", "where", "extra", "expected"],
+        [
+            [
+                "A", "B", None, None,
+                "SELECT A FROM B"
+            ], [
+                "A", "B B", None, None,
+                "SELECT A FROM 'B B'"
+            ], [
+                "A", "B-B", Where("C", 1), None,
+                "SELECT A FROM [B-B] WHERE C = 1"
+            ], [
+                "A", "B-B", Where("C", 1, cmp_operator=">"), "ORDER BY D",
+                "SELECT A FROM [B-B] WHERE C > 1 ORDER BY D"
+            ],
+        ])
+    def test_normal(self, select, table, where, extra, expected):
+        assert_query_item(Select(select, table, where, extra), expected)
+
+    @pytest.mark.parametrize(
+        ["select", "table", "where", "extra", "expected"], [
+            ["A", None, None, None, InvalidTableNameError],
+            ["", "B", None, None, ValueError],
+            [None, None, None, None, InvalidTableNameError],
+            [None, "B", None, None, ValueError],
+            [nan, None, None, None, InvalidTableNameError],
+            [nan, nan, None, None, TypeError],
+            [nan, nan, nan, None, TypeError],
+            [nan, nan, nan, nan, TypeError],
+        ])
+    def test_exception(self, select, table, where, extra, expected):
+        with pytest.raises(expected):
+            "{}".format(Select(select, table, where, extra))
+
+
+class Test_And(object):
+
+    @pytest.mark.parametrize(["where_list", "expected"], [
+        [
+            [
+                Where("hoge", "abc"),
+                Where("bar", 100, ">"),
+            ],
+            "hoge = 'abc' AND bar > 100"
+        ], [
+            [],
+            ""
+        ]
+    ])
+    def test_normal(self, where_list, expected):
+        assert_query_item(And(where_list), expected)
+
+
+class Test_make_index_name(object):
+    SANITIZE_CHAR_LIST = [
+        ":", "*", "?", "|", "%", "/", "(", ")", "[", "]", "<", ">", ".", ";",
+        "'", '"', "!", "\\", "#", " ", "-", "+", "=", " ",
+        "\t", "\n", "\r", "\f", "\v",
+    ]
+
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            ["AAA{:s}".format(re.escape(c)), "AAA"] for c in SANITIZE_CHAR_LIST
+        ] + [
+            ["{:s}BBB".format(re.escape(c)), "BBB"] for c in SANITIZE_CHAR_LIST
+        ] + [
+            [
+                "%a/b(c)d[e]f<g>h.i;j'k!l\\m#n _o-p+q=r\nstrvwxyz" +
+                os.linesep,
+                "abcdefghijklmn_opqrstrvwxyz"
+            ]
+        ])
+    def test_normal(self, value, expected):
+        assert re.search("\w+_index_\w{4}", make_index_name(value, value), re.ASCII)
+
+    def test_normal_symbol(self):
+        assert make_index_name("table", "ABCD>8.5") != make_index_name("table", "ABCD<8.5")
