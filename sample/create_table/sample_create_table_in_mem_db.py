6f657d5cdbb1b301a4a998260f26d6e97ee1eec7
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-05 15:13:42 +0900

    Update examples

--- sample/create_table/sample_create_table_in_mem_db.py
+++ sample/create_table/sample_create_table_in_mem_db.py
@@ -22,11 +22,12 @@ con.create_table_from_data_matrix(
     attr_name_list=["attr_a", "attr_b", "attr_c", "attr_d", "attr_e"],
     data_matrix=data_matrix)
 
+# display data type for each column in the table -----
+print(con.schema_extractor.fetch_table_schema(table_name).dumps())
+
 # display values in the table -----
-print(con.fetch_attr_name_list(table_name))
+print("records:")
 result = con.select(select="*", table_name=table_name)
 for record in result.fetchall():
     print(record)
 
-# display data type for each column in the table -----
-print(json.dumps(con.fetch_attr_type(table_name), indent=4))
