c631e67993e383622f02e03f44367e6d355e396a
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-05 15:25:42 +0900

    Remove unused imports

--- sample/create_table/sample_create_table_from_data_matrix.py
+++ sample/create_table/sample_create_table_from_data_matrix.py
@@ -3,8 +3,6 @@
 
 from __future__ import print_function
 
-import json
-
 from simplesqlite import SimpleSQLite
 
 
--- sample/create_table/sample_create_table_in_mem_db.py
+++ sample/create_table/sample_create_table_in_mem_db.py
@@ -3,8 +3,6 @@
 
 from __future__ import print_function
 
-import json
-
 import simplesqlite
 
 
