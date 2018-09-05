af2169f458c7519f59fafb7ce678a9429549f6fd
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-09-05 22:40:32 +0900

    Update an example

--- sample/orm/simple_orm.py
+++ sample/orm/simple_orm.py
@@ -14,7 +14,7 @@ from simplesqlite.model import Integer, Model, Real, Text
 
 
 class Sample(Model):
-    foo_id = Integer(not_null=True)
+    foo_id = Integer(primary_key=True)
     name = Text(not_null=True, unique=True)
     value = Real()
 
@@ -24,13 +24,14 @@ def main():
 
     Sample.attach(con)
     Sample.create()
-    Sample.insert(Sample(foo_id=11, name="abc", value=0.1))
-    Sample.insert(Sample(foo_id=22, name="xyz", value=1.11))
+    Sample.insert(Sample(name="abc", value=0.1))
+    Sample.insert(Sample(name="xyz", value=1.11))
+    Sample.insert(Sample(name="bar", value=2.22))
 
     print(Sample.fetch_schema().dumps())
     print("records:")
     for record in Sample.select():
-        print("  {}".format(record))
+        print("    {}".format(record))
 
     return 0
 
