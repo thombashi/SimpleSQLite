a9d3b85b59ad61a279daeb916241721f66671ece
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-02-23 07:52:05 +0900

    Rename variables

--- test/test_convertor.py
+++ test/test_convertor.py
@@ -12,30 +12,30 @@ import pytest
 from simplesqlite.converter import RecordConvertor
 
 
-attr_list_2 = ["attr_a", "attr_b"]
-attr_list_3 = ["attr_a", "attr_b", "attr_c"]
+attrs_2 = ["attr_a", "attr_b"]
+attrs_3 = ["attr_a", "attr_b", "attr_c"]
 
-NamedTuple2 = namedtuple("NamedTuple2", " ".join(attr_list_2))
-NamedTuple3 = namedtuple("NamedTuple3", " ".join(attr_list_3))
+NamedTuple2 = namedtuple("NamedTuple2", " ".join(attrs_2))
+NamedTuple3 = namedtuple("NamedTuple3", " ".join(attrs_3))
 
 
 class Test_RecordConvertor_to_record(object):
     @pytest.mark.parametrize(
         ["attr_names", "value", "expeted"],
         [
-            [attr_list_2, [5, 6], [5, 6]],
-            [attr_list_2, (5, 6), [5, 6]],
-            [attr_list_2, {"attr_a": 5, "attr_b": 6}, [5, 6]],
-            [attr_list_2, {"attr_a": 5, "attr_b": 6, "not_exist_attr": 100}, [5, 6]],
-            [attr_list_2, {"attr_a": 5}, [5, None]],
-            [attr_list_2, {"attr_b": 6}, [None, 6]],
-            [attr_list_2, {}, [None, None]],
-            [attr_list_2, NamedTuple2(5, 6), [5, 6]],
-            [attr_list_2, NamedTuple2(5, None), [5, None]],
-            [attr_list_2, NamedTuple2(None, 6), [None, 6]],
-            [attr_list_2, NamedTuple2(None, None), [None, None]],
-            [attr_list_2, NamedTuple3(5, 6, 7), [5, 6]],
-            [attr_list_3, NamedTuple3(5, 6, 7), [5, 6, 7]],
+            [attrs_2, [5, 6], [5, 6]],
+            [attrs_2, (5, 6), [5, 6]],
+            [attrs_2, {"attr_a": 5, "attr_b": 6}, [5, 6]],
+            [attrs_2, {"attr_a": 5, "attr_b": 6, "not_exist_attr": 100}, [5, 6]],
+            [attrs_2, {"attr_a": 5}, [5, None]],
+            [attrs_2, {"attr_b": 6}, [None, 6]],
+            [attrs_2, {}, [None, None]],
+            [attrs_2, NamedTuple2(5, 6), [5, 6]],
+            [attrs_2, NamedTuple2(5, None), [5, None]],
+            [attrs_2, NamedTuple2(None, 6), [None, 6]],
+            [attrs_2, NamedTuple2(None, None), [None, None]],
+            [attrs_2, NamedTuple3(5, 6, 7), [5, 6]],
+            [attrs_3, NamedTuple3(5, 6, 7), [5, 6, 7]],
         ],
     )
     def test_normal(self, attr_names, value, expeted):
@@ -43,7 +43,7 @@ class Test_RecordConvertor_to_record(object):
 
     @pytest.mark.parametrize(
         ["attr_names", "value", "expeted"],
-        [[None, [5, 6], TypeError], [attr_list_2, None, ValueError], [None, None, TypeError]],
+        [[None, [5, 6], TypeError], [attrs_2, None, ValueError], [None, None, TypeError]],
     )
     def test_exception(self, attr_names, value, expeted):
         with pytest.raises(expeted):
@@ -55,7 +55,7 @@ class Test_RecordConvertor_to_records(object):
         ["attr_names", "value", "expeted"],
         [
             [
-                attr_list_2,
+                attrs_2,
                 [
                     [1, 2],
                     (3, 4),
@@ -75,7 +75,7 @@ class Test_RecordConvertor_to_records(object):
 
     @pytest.mark.parametrize(
         ["attr_names", "value", "expeted"],
-        [[None, [5, 6], TypeError], [attr_list_2, None, TypeError], [None, None, TypeError]],
+        [[None, [5, 6], TypeError], [attrs_2, None, TypeError], [None, None, TypeError]],
     )
     def test_exception(self, attr_names, value, expeted):
         with pytest.raises(expeted):
