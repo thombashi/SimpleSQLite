7aa419749dc53b6a525428114ba3d07752e7b221
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-05-11 21:09:12 +0900

    Bump versionAdd support for Python 3.8

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Database",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
