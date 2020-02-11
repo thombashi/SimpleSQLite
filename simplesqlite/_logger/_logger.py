142dcaafcf62e4c802a91cab0d3cfa20e4eb0e57
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-11 20:23:39 +0900

    Modify set_logger

--- simplesqlite/_logger/_logger.py
+++ simplesqlite/_logger/_logger.py
@@ -13,7 +13,6 @@ from ._null_logger import NullLogger
 
 
 MODULE_NAME = "simplesqlite"
-_is_enable = False
 
 
 try:
@@ -21,28 +20,26 @@ try:
 
     logger.disable(MODULE_NAME)
 except ImportError:
-    logger = NullLogger()
+    logger = NullLogger()  # type: ignore
 
 
-def set_logger(is_enable):
-    global _is_enable
-
-    if is_enable == _is_enable:
-        return
-
+def set_logger(is_enable, propagation_depth=1):
     if is_enable:
         logger.enable(MODULE_NAME)
     else:
         logger.disable(MODULE_NAME)
 
-    tabledata.set_logger(is_enable)
-    sqliteschema.set_logger(is_enable)
+    if propagation_depth <= 0:
+        return
+
+    tabledata.set_logger(is_enable, propagation_depth - 1)
+    sqliteschema.set_logger(is_enable, propagation_depth - 1)
 
     try:
         import pytablereader
 
-        pytablereader.set_logger(is_enable)
-    except ImportError:
+        pytablereader.set_logger(is_enable, propagation_depth - 1)
+    except (ImportError, TypeError):
         pass
 
 
